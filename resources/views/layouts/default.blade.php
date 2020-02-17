96243bacf9aecf756c78b8bfe4667f5d0cea69f4
taizt <494762513@qq.com>
2020-02-17 21:29:00 +0800

    weibo

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title','Weibo App') - Laravel 教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    @include('layouts._header')
+    <div class="container">
+        <div class="offset-md-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
+</body>
+</html>
