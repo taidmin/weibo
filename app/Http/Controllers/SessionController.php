169ea38bf9c6e14842fdd61fe2270b00edc8d639
taizt <494762513@qq.com>
2020-02-19 15:42:20 +0800

    访问策略

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,6 +7,14 @@ use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
+    public function __construct()
+    {
+        // 只让未登录用户访问登录页面
+        $this->middleware('guest',[
+            'only' => 'create'
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -22,8 +30,8 @@ class SessionController extends Controller
         if(Auth::attempt($credentials, $request->has('remember'))){
             // 登录成功后的相关操作
             session()->flash('success', '欢迎回来！');
-
-            return redirect()->route('users.show', [Auth::user()]);
+            $fallback = route('users.show', [Auth::user()]);
+            return redirect()->intended($fallback);  // intended 将页面重定向到上一次请求尝试访问的页面
 
         }else{
             // 登录失败后的相关操作
